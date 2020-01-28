b0e3f5afb96e10117761c33a007e1253b20ad30c
66hakeem <66hakeem@gmail.com>
2020-01-28 16:18:20 +0300

    Add functionality for editing an entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -58,7 +58,7 @@ def new_entry(request, topic_id):
 
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
-    entry = Entry.objects,get(id=entry_id)
+    entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
 
     if request.method != 'POST':
