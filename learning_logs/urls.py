749d87bfaacebd8058f2421534fa02c5765b950c
66hakeem <66hakeem@gmail.com>
2020-01-28 11:14:19 +0300

    Adds Views for Topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,7 +3,16 @@
 from django.urls import path
 from . import views
 
+app_name = 'learning_logs'
+
 urlpatterns = [
     #Homepage
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    # Show all topics
+    path('topics/', views.topics, name='topics'),
+
+    # detail page for single topic
+    path('topics/<topic_id>', views.topic, name='topic')
 ]
+
