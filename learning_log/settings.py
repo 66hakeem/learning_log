c950d74740307391b3aa9b9e14471b3037595a75
66hakeem <66hakeem@gmail.com>
2020-02-12 15:43:14 +0300

    Adds logout functionality

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
 
     #My apps
     'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
