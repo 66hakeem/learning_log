08ace7c0237ac89e8c086cd058b8e818c03bbde7
66hakeem <66hakeem@gmail.com>
2020-02-12 17:02:38 +0300

    Specifies Topics to Users

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
+
 def logout_view(request):
     """Log the user out"""
     logout(request)
@@ -16,6 +17,16 @@ def register(request):
         form = UserCreationForm()
     else:
         #Process completed form
-        
+        form = UserCreationForm(data=request.POST)
 
-# Create your views here.
+        if form.is_valid():
+            new_user = form.save()
+            #Log the user in and then redirect to the home page
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
+
+
